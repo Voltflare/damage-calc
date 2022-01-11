8a0b448bccccf2616521ba4bc8a345bf0466b7df
Nathan Hites <41966910+Voltflare@users.noreply.github.com>
2022-01-11 16:23:12 -0600

    Added Species Sets up to 1/11/2022

--- js/data/sets/gen8.js
+++ js/data/sets/gen8.js
@@ -50,4 +50,344 @@ var SETDEX_SS = {
     "moves":["Zing Zap","Earthquake","Hammer Arm","Giga Impact"]
    }
   },
+    "Espeon-Psychic/Fire":
+  {"Daedalus [-HP]":
+   {"level":100,
+    "ability":"Fire Meld",
+    "item":"None",
+    "nature":"Sassy",
+    "evs":{"hp":100, "sd":100},
+    "moves":["Future Sight","Swift","Helping Hand","Copycat"]
+   }
+  },
+    "Porygon-Z-Normal/Water":
+  {"Lizzy":
+   {"level":100,
+    "ability":"Steam Engine",
+    "item":"None",
+    "nature":"Sassy",
+    "evs":{"sa":200, "sd":100},
+    "moves":["Psychic","Scald","Reflect","Ally Switch"]
+   }
+  },
+    "Charizard-Electric/Flying":
+  {"Draken":
+   {"level":100,
+    "ability":"Transistor",
+    "item":"None",
+    "nature":"Quiet",
+    "evs":{"hp":100, "sa":200},
+    "moves":["Air Slash","Thunderbolt","Discharge","Dragon Claw"]
+   }
+  },
+    "Zoroark-Dark/Ghost":
+  {"Zyra":
+   {"level":100,
+    "ability":"Illusion",
+    "item":"None",
+    "nature":"Naive",
+    "evs":{"sa":200, "sp":100},
+    "moves":["Shadow Claw","Night Daze","Nasty Plot","Confuse Ray"]
+   }
+  },
+    "Luxray-Electric/Fire":
+  {"Vash":
+   {"level":100,
+    "ability":"Rivalry",
+    "item":"None",
+    "nature":"Rash",
+    "evs":{"hp":100, "sa": 100, "sd":100},
+    "moves":["Incinerate","Discharge","Roar","Scary Face"]
+   }
+  },
+    "Excadrill":
+  {"Garlic":
+   {"level":100,
+    "ability":"Mold Breaker",
+    "item":"None",
+    "nature":"Jolly",
+    "evs":{"at":100},
+    "moves":["Earthquake","Rock Slide","Swords Dance","Iron Head"]
+   }
+  },
+    "Porygon":
+  {"Scaff":
+   {"level":100,
+    "ability":"Download",
+    "item":"None",
+    "nature":"Careful",
+    "evs":{"hp":100, "sd":100},
+    "moves":["Psybeam","Agility","Conversion 2","Recover"]
+   }
+  },
+    "Dragonite":
+  {"Helix [+HP]":
+   {"level":100,
+    "ability":"Multiscale",
+    "item":"None",
+    "nature":"Jolly",
+    "evs":{"hp":200, "at": 100},
+    "moves":["Roost","Light Screen","Breaking Swipe","Wrap"]
+   }
+  },
+    "Gabite-Dragon/Ice":
+  {"Odysseus [-HP]":
+   {"level":100,
+    "ability":"Multiscale",
+    "item":"None",
+    "nature":"Rash",
+    "evs":{"hp":200, "at": 100},
+    "moves":["Avalanche","Hail","Haze","Dragon Breath"]
+   }
+  },
+    "Lucario":
+  {"Weyland [+HP]":
+   {"level":100,
+    "ability":"Steadfast",
+    "item":"None",
+    "nature":"Serious",
+    "evs":{"df":200, "sa":100},
+    "moves":["Focus Blast","Metal Sound","Heal Pulse","Meteor Mash"]
+   }
+  },
+    "Eiscue":
+  {"Oxford":
+   {"level":100,
+    "ability":"Ice Face",
+    "item":"None",
+    "nature":"Timid",
+    "evs":{"hp":200, "sp":100},
+    "moves":["Icicle Crash","Icy Wind","Headbutt","Hail"]
+   }
+  },
+    "Obstagoon":
+  {"Loralei":
+   {"level":100,
+    "ability":"Defiant",
+    "item":"None",
+    "nature":"Quirky",
+    "evs":{"sp":100, "sp":100},
+    "moves":["Obstruct","Counter","Snarl","Hyper Voice"]
+   }
+  },
+    "Luxray-Rock":
+  {"Grace [+HP]":
+   {"level":100,
+    "ability":"Solid Rock",
+    "item":"None",
+    "nature":"Naive",
+    "evs":{"hp":100, "at":100},
+    "moves":["Iron Defense","Rock Slide","Stealth Rock","Brick Break"]
+   }
+  },
+    "Braixen":
+  {"Grave Lohl":
+   {"level":100,
+    "ability":"Magician",
+    "item":"None",
+    "nature":"Careful",
+    "evs":{"hp":100, "df":100, "sd":100},
+    "moves":["Echoed Voice","Shock Wave","Lucky Chant","Flame Charge"]
+   }
+  },
+    "Girafarig-Dragon/Psychic":
+  {"Neroh Corsair":
+   {"level":100,
+    "ability":"Sheer Force",
+    "item":"None",
+    "nature":"Jolly",
+    "evs":{"at":100, "sd":100},
+    "moves":["Dragon Rush","Zen Headbutt","Telekinesis","Dragon Dance"]
+   }
+  },
+    "Jigglypuff-Normal/Dark":
+  {"Jolanda Smit":
+   {"level":100,
+    "ability":"Cute Charm",
+    "item":"None",
+    "nature":"Jolly",
+    "evs":{"hp":100},
+    "moves":["Sing","Attract","Echoed Voice","Fake Tears"]
+   }
+  },
+    "Salandit":
+  {"Iris":
+   {"level":100,
+    "ability":"Corrosion",
+    "item":"None",
+    "nature":"Quirky",
+    "evs":{"hp":200},
+    "moves":["Toxic","Venoshock","Venom Drench","Flamethrower"]
+   }
+  },
+    "Indeedee":
+  {"Belrephon":
+   {"level":100,
+    "ability":"Psychic Surge",
+    "item":"None",
+    "nature":"Careful",
+    "evs":{"hp":200, "sd":100},
+    "moves":["Expanding Force","Calm Mind","Aromatherapy","Dazzling Gleam"]
+   }
+  },
+    "Raichu":
+  {"Drifter [-HP]":
+   {"level":100,
+    "ability":"Lightning Rod",
+    "item":"None",
+    "nature":"Impish",
+    "evs":{"at":100, "sp":100},
+    "moves":["Electro Ball","Iron Tail","Volt Tackle","Laser Focus"]
+   }
+  },
+    "Stufful-Fire/Fighting":
+  {"Spike":
+   {"level":100,
+    "ability":"Fluffy",
+    "item":"None",
+    "nature":"Naughty",
+    "evs":{"at":100, "df":100},
+    "moves":["Will-O-Wisp","Fire Lash","Payback","Thunder Punch"]
+   }
+  },
+    "Lucario-Ghost/Steel":
+  {"Ninghe":
+   {"level":100,
+    "ability":"Inner Focus",
+    "item":"None",
+    "nature":"Careful",
+    "evs":{"hp":100, "sd":100},
+    "moves":["Shadow Ball","Calm Mind","Shadow Punch","Meteor Mash"]
+   }
+  },
+    "Zangoose":
+  {"Zoja Kalnins":
+   {"level":100,
+    "ability":"Toxic Boost",
+    "item":"None",
+    "nature":"Brave",
+    "evs":{"at":100, "sp":100},
+    "moves":["Hone Claws","Slash","Night Slash","Revenge"]
+   }
+  },
+    "Ralts":
+  {"Paklin Costur":
+   {"level":100,
+    "ability":"Trace",
+    "item":"None",
+    "nature":"Timid",
+    "evs":{"sa":100, "sp":100},
+    "moves":["Disarming Voice","Psybeam","Teleport","Shadow Sneak"]
+   }
+  },
+    "Skarmory-Steel/Electric":
+  {"Briar":
+   {"level":100,
+    "ability":"Keen Eye",
+    "item":"None",
+    "nature":"Careful",
+    "evs":{"sp":200},
+    "moves":["Steel Wing","Spark","Autotomize","Spikes"]
+   }
+  },
+    "Ditto-Bug":
+  {"Cotton":
+   {"level":100,
+    "ability":"The Fae",
+    "item":"None",
+    "nature":"Naive",
+    "evs":{"sp":200},
+    "moves":["Pollen Puff"]
+   }
+  },
+    "Mienshao-Dragon/Fighting":
+  {"Gila Hearth":
+   {"level":100,
+    "ability":"Battle Armor",
+    "item":"None",
+    "nature":"Jolly",
+    "evs":{"at":200, "sp":100},
+    "moves":["Bounce","High Jump Kick","Dragon Rush","Bulk Up"]
+   }
+  },
+    "Lycanroc-Midday":
+  {"Cal":
+   {"level":100,
+    "ability":"Keen Eye",
+    "item":"None",
+    "nature":"Jolly",
+    "evs":{"hp":100, "sd":200},
+    "moves":["Stone Edge","Protect","Rock Tomb"]
+   }
+  },
+    "Leafeon":
+  {"Sol [-HP]":
+   {"level":100,
+    "ability":"Chlorophyll",
+    "item":"None",
+    "nature":"Brave",
+    "evs":{"at":100, "df":100},
+    "moves":["Sunny Day","Razor Leaf","Dig","Synthesis"]
+   }
+  },
+    "Garchomp-Dragon/Steel":
+  {"Magnus [HP+]":
+   {"level":100,
+    "ability":"Iron Barbs",
+    "item":"None",
+    "nature":"Jolly",
+    "evs":{"at":100, "df":200},
+    "moves":["Meteor Mash","Dragon Rush","Iron Defense","Sandstorm"]
+   }
+  },
+    "Slowking":
+  {"Seaghdha":
+   {"level":100,
+    "ability":"Own Tempo",
+    "item":"None",
+    "nature":"Calm",
+    "evs":{"sp":100, "sd":200},
+    "moves":["Heal Pulse","Calm Mind","Trick Room","Psychic"]
+   }
+  },
+    "Metang-Rock/Psychic":
+  {"Ricochet":
+   {"level":100,
+    "ability":"Clear Body",
+    "item":"None",
+    "nature":"Mild",
+    "evs":{"df":100},
+    "moves":["Power Gem","Psychic","Magnet Rise","Rock Polish"]
+   }
+  },
+    "Pansear":
+  {"Dubstep":
+   {"level":100,
+    "ability":"Blaze",
+    "item":"None",
+    "nature":"Bold",
+    "evs":{"hp":100, "sd":100},
+    "moves":["Psybeam","Agility","Conversion","Recover"]
+   }
+  },
+    "Aggron":
+  {"Coal":
+   {"level":100,
+    "ability":"Heavy Metal",
+    "item":"None",
+    "nature":"Careful",
+    "evs":{"at":200, "df":100},
+    "moves":["Sunny Day","Fire Punch","Autotomize","Iron Tail"]
+   }
+  },
+    "Sliggoo-Dragon/Steel":
+  {"Ismini":
+   {"level":100,
+    "ability":"Mercury",
+    "item":"None",
+    "nature":"Timid",
+    "evs":{"hp":100},
+    "moves":["Protect","Mirror Shot","Rain Dance","Acid Spray"]
+   }
+  },
 }; //end set_dex
