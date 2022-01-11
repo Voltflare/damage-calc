64d2454942176074a04786d0e9f755ce148ba2c6
Nathan Hites <41966910+Voltflare@users.noreply.github.com>
2022-01-11 10:45:32 -0600

    Added Blot/Narset Sets

--- js/data/sets/gen8.js
+++ js/data/sets/gen8.js
@@ -19,5 +19,25 @@ var SETDEX_SS = {
     "evs":{"sa":200},
     "moves":["Iron Tail","Blizzard","Thunder","Coil"]
    }
-  }
+  },
+    "Blot":
+  {"Non-Islander Smeargle":
+   {"level":100,
+    "ability":"Moody",
+    "item":"None",
+    "nature":"Careful",
+    "evs":{"df":100, "sd":100},
+    "moves":["Poltergeist","Phantom Force","Sketch","Sketch"]
+   }
+  },
+    "Narset":
+  {"Cat with Swords":
+   {"level":100,
+    "ability":"Keen Eye",
+    "item":"None",
+    "nature":"Lonely",
+    "evs":{"hp":100, "at":100},
+    "moves":["Slash","U-turn","Hypnosis","Aerial Ace"]
+   }
+  },
 }; //end set_dex
