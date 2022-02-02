e8224633f484dccec0a70b72e741352e97f5c31b
Nathan Hites <41966910+Voltflare@users.noreply.github.com>
2022-02-01 22:11:04 -0600

    Update gen8.js

--- calc/mechanics/gen8.js
+++ calc/mechanics/gen8.js
@@ -453,15 +453,9 @@ function calculateGen8(gen, attacker, defender, move, field) {
         (!move.makesContact || attacker.hasAbility('Long Reach')) &&
         move.type === 'Fire') {
         bpMods.push(0x2000);
-        //Special version of Fluffy to keep Pokemon that add a resistance to it via TS, weak to Fire
-        if ((defender.hasAbility('Fluffy Fuel')) &&
-        (!move.makesContact || attacker.hasAbility('Long Reach')) &&
-        move.type === 'Fire') {
-            bpMods.push(0x2000);
-        }
         description.defenderAbility = defender.ability;
     }
-    if ((defender.hasAbility('Fluffy') || defender.hasAbility('Fur Coat') || defender.hasAbility('Fluffy Fuel')) &&
+    else if ((defender.hasAbility('Fluffy') || defender.hasAbility('Fur Coat')) &&
         move.makesContact &&
         !attacker.hasAbility('Long Reach') &&
         move.type !== 'Fire') {
