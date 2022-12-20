3e282f375e9aff33e1aa34de003ca6b228daf266
Nathan Hites <41966910+Voltflare@users.noreply.github.com>
2022-12-19 19:50:27 -0600

    Update species.js

--- calc/data/species.js
+++ calc/data/species.js
@@ -9224,7 +9224,7 @@ var SS_PATCH = {
         abilities: {0: 'Sheer Force'},
         nfe: true
     },
-        'Farigiraf-Dragon/Psychic': {
+        'Farigiraf-Dragon/Psychic-Neroh': {
         types: ['Dragon', 'Psychic'],
         bs: { hp: 120, at: 100, df: 70, sa: 110, sd: 70, sp: 60 },
         weightkg: 160,
